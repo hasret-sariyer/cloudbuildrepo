084f3f9e0149e1961265cee5ae1877212a94de62
hasretsariyer <hasretsariyer@gmail.com>
2020-01-27 12:04:16 +0300

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,9 +1,10 @@
 
 touch $ANDROID_HOME/hasret-test.txt
 ls $ANDROID_HOME/platforms
-echo "Pre-build custom script1"
+echo "Pre-build custom script1..."
 
 # rm -rf $ANDROID_HOME
 echo "====================================="
-pwd
+echo "Current working directory: "
+echo $(pwd)
 echo "====================================="
