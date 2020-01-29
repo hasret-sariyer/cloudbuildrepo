3ffd817190db3450c7de02296f48682159f2963c
hasretsariyer <hasretsariyer@gmail.com>
2020-01-29 09:40:46 +0300

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -2,6 +2,7 @@
 touch $ANDROID_HOME/hasret-test.txt
 ls $ANDROID_HOME/platforms
 echo "Pre-build custom script1..."
+ls $ANDROID_HOME/system-images
 
 # rm -rf $ANDROID_HOME
 echo "====================================="
